f227107335d47a09c084430fa5c5d635a6c695c0
oPatussi <opatussi@outlook.com>
2022-08-09 21:33:26 -0300

    Aula 09/08/2022

--- loja-backend/src/server.ts
+++ loja-backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import { AppDataSource } from "./data-source"
 
 //Instacia uma aplicação express
 const app = express();
@@ -6,6 +8,18 @@ const app = express();
 //Determina a porta de execução
 const PORT = 3300;
 
-app.listen(PORT, () => {
-    console.log(`Running in port ${PORT}`)
-});
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Tenta conexão, caso erro, mostra log
+AppDataSource.initialize()
+    .then(() =>{
+        app.listen(PORT, () => {
+            console.log(`Running in port ${PORT}`);
+        })
+
+    })
+    .catch(error => {
+        console.log('Ops, não conectei no banco de dados', error);
+    });
